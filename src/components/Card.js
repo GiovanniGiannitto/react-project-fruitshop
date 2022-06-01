bd3a0888b368d796b11c039e714a5e3a6e7a7874
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-06-01 16:05:33 +0200

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react";
 
-export function Card(){
-    const [data, setData] = useState()
+export function Card() {
+  const fruits = []
+  const [data, setData] = useState(fruits);
 
-    
+  useEffect(() => {
+      
+    fetch(`https://fruits-develhope.herokuapp.com/api`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setData(json);
+        console.log(json);
+      });
+  }, []);
 
-
-    return(
-        <div>
-
-        </div>
-    )
+  return (
+    <div>
+      { 
+        data.map((element, index) => (
+          <li key={index}>{element}</li>
+        ))}
+    </div>
+  );
 }
