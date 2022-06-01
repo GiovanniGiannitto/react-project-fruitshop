01ef66333688a9aef92f8fefaee511a0c2c0ef13
Nicholas Pesaresi <nicholaspesaresi@gmail.com>
2022-06-01 17:44:42 +0200

    cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 export function Card() {
-  const fruits = []
-  const [data, setData] = useState(fruits);
+  const [data, setData] = useState();
 
   useEffect(() => {
-      
     fetch(`https://fruits-develhope.herokuapp.com/api`)
       .then((response) => {
         return response.json();
@@ -18,9 +16,10 @@ export function Card() {
 
   return (
     <div>
-      { 
-        data.map((element, index) => (
-          <li key={index}>{element}</li>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {data &&
+        data.fruits.map((element, index) => (
+          <li key={index}>{element.name}</li>
         ))}
     </div>
   );
